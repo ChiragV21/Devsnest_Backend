e38ec2966625bfd2d12e0a5ab1fdbad4c198bf2a
Chirag Varshney <55586574+ChiragV21@users.noreply.github.com>
2021-09-24 18:24:34 +0530

    Add files via upload

---
+++ Day-7/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.listen(5000);
+
+app.get("/withjson", (req, res) => {
+    res.json({ a: 1 });
+})
+
+app.get("/status", (req, res) => {
+    res.status(200);   
+    res.send("hello");
+})
+
+app.get("/statusi", (req, res) => {               
+    res.sendStatus(500).json({ a: 1 });
+})
+
+app.get("/query", (req, res) => {
+    res.send(req.query.k)
+})
+
+app.get("/users/:userID/book/:bookID", (req, res) => {
+    res.send(req.params);
+})
