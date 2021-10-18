6edd2208ccc29060079d410bf96ee51c11ca5d7d
Chirag Varshney <55586574+ChiragV21@users.noreply.github.com>
2021-10-18 22:49:04 +0530

    Update app.js

--- Day-14/app.js
+++ Day-14/app.js
@@ -11,8 +11,6 @@ const { redisClient, RedisStore, session } = require("./database/redis");
 require("./database/mongo");
 
 var app = express();
-
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -44,16 +42,11 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
 
 module.exports = app;
-//toxin
